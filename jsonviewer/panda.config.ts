35e1d2cefbe3de4dc059bee29ad7e54d2371d276
meluiz <me.luizfelipe@gmail.com>
2023-10-18 17:56:39 -0300

    chore(panda.config.ts): update panda.config.ts to include new presets and remove unused code
    The panda.config.ts file was updated to include the following changes:
    - Added the `theme`, `patterns`, `globalCss`, and `staticCss` imports from the `@lib/pandacss` module.
    - Updated the `defineConfig` function to include the new imports.
    - Removed the unused `exclude` property.
    - Set the `preflight` property to `true` to enable preflight styles.
    - Updated the `outdir` property to `'pandacss'`.
    - Set the `jsxFramework` property to `'react'`.
    - Updated the `include` property to include all files with the extensions `.ts` and `.tsx`.
    - Added the `@park-ui/presets` preset to the `presets` array.
    
    These changes were made to update the configuration of the Panda CSS library and remove unused code to improve maintainability and performance.

--- jsonviewer/panda.config.ts
+++ jsonviewer/panda.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from '@pandacss/dev'
 
+import { globalCss, patterns, staticCss, theme } from '@lib/pandacss'
+
 export default defineConfig({
+  theme,
+  patterns,
+  globalCss,
+  staticCss,
   exclude: [],
   preflight: true,
   outdir: 'pandacss',
   jsxFramework: 'react',
-  presets: ['@pandacss/preset-base'],
   include: ['./src/**/*.{ts,tsx}'],
+  presets: ['@pandacss/preset-base', '@park-ui/presets'],
 })
